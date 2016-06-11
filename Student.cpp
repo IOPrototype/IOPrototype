925892843f9370d54af951a7e9b96aef535dd104
IOPrototype <michal.watrobka2@o2.pl>
2016-06-11 21:59:29 +0200

    3

--- Student.cpp
+++ Student.cpp
@@ -12,12 +12,21 @@ Student::Student(std::string& _login,std::string& _password,std::string& _name,s
 	name=_name;
 	surname=_surname;
 	id=_id;
+	type=2;
 	for(int j=0;j<icourses.size();j++)
 		for(int i=0;i<_courses.size();i++)
 			if(_courses[i].getID()==icourses[j]) courses.push_back(_courses[i].getID());
 
 }
-
+Student::Student(std::string& _login,std::string& _password,std::string& _name,std::string& _surname,int _id)
+{
+	login=_login;
+	password=_password;
+	name=_name;
+	surname=_surname;
+	id=_id;
+	type=2;
+}
 
 Student::~Student(void)
 {
